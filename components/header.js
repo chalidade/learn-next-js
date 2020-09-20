610a57bd72e78ab0a9f7be2a94ac8402f8709b59
Chalid Ade Rahman <chalidade@gmail.com>
2020-09-20 19:34:56 +0700

    complete admin lte

--- components/header.js
+++ components/header.js
@@ -6,7 +6,11 @@ const Header = () => (
     <title>Lerero Project</title>
     <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css" />
     <link rel="stylesheet" href="dist/css/adminlte.min.css" />
+    <link rel="stylesheet" href="dist/css/custom.css" />
     <link href="dist/css/google-font.css" rel="stylesheet" />
+    <script src="plugins/jquery/jquery.min.js"></script>
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="dist/js/adminlte.min.js"></script>
   </header>
 );
 
