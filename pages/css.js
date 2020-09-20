59bceb0e4d0c0e4974bfb5ab35826ade7fcee6cf
Chalid Ade Rahman <chalidade@gmail.com>
2020-09-20 18:21:08 +0700

    style component

--- pages/css.js
+++ pages/css.js
@@ -5,6 +5,21 @@ const CSS = () => (
     <Header />
     <div className="title">
       <h1> CSS </h1>
+      <style jsx>
+        {`
+          .title {
+            background: #eee;
+            padding: 100px;
+
+            text-align: center;
+            transition: 100ms ease-in background;
+          }
+
+          .title:hover {
+            background: $hover-color;
+          }
+        `}
+      </style>
     </div>
   </>
 );
